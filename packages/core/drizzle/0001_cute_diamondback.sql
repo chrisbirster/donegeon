0616ea07793abacbe3a52816690400182d7547c8
Chris Birster <christopher.birster@gmail.com>
2025-06-10 20:13:24 -0400

    feat: add db updated schema

---
+++ packages/core/drizzle/0001_cute_diamondback.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `users` (
+	`id` text PRIMARY KEY NOT NULL,
+	`name` text NOT NULL,
+	`email` text NOT NULL,
+	`email_verified` integer NOT NULL,
+	`image` text,
+	`created_at` integer NOT NULL,
+	`updated_at` integer NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);
