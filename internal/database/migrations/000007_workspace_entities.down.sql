99c0c66c4f3f5d46d1047337e657ec82494aca1d
Chris Birster <christopher.birster@gmail.com>
2026-08-23 21:37:18 -0400

    chore: neutralize workspace migration naming

---
+++ internal/database/migrations/000007_workspace_entities.down.sql
@@ -0,0 +1,6 @@
+DROP INDEX IF EXISTS idx_workspace_invitations_workspace_id;
+DROP INDEX IF EXISTS idx_workspace_users_workspace_id;
+
+DROP TABLE IF EXISTS workspace_invitations;
+DROP TABLE IF EXISTS workspace_users;
+DROP TABLE IF EXISTS workspaces;
