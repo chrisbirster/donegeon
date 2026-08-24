f430fb58ce76c63c825b2da676f872f439a1c9e4
Chris Birster <christopher.birster@gmail.com>
2026-08-23 21:37:30 -0400

    chore: neutralize workspace migration naming

--- internal/database/migrations/000007_todoist_compat_entities.down.sql
+++
@@ -1,6 +0,0 @@
-DROP INDEX IF EXISTS idx_workspace_invitations_workspace_id;
-DROP INDEX IF EXISTS idx_workspace_users_workspace_id;
-
-DROP TABLE IF EXISTS workspace_invitations;
-DROP TABLE IF EXISTS workspace_users;
-DROP TABLE IF EXISTS workspaces;
